31d9d089d90035c5cd9042e67f5173889e89d6ea
BrianDuemmer <bduemmer1@gmail.com>
2019-04-12 18:40:34 -0400

    Created can_hal.c

---
+++ src/device/can_hal.c
@@ -0,0 +1,30 @@
+/*
+ * can_hal.c
+ *
+ *	Contains implementation for the cvnp_hal system
+ *
+ *  Created on: Apr 12, 2019
+ *      Author: Duemmer
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <driverlib/can.h>
+#include <driverlib/gpio.h>
+
+#include "../util.h"
+#include "../fault.h"
+#include "../cvnp/cvnp_hal.h"
+
+void _can_intHandler() {
+
+}
+
+
+/**
+ * Initializes the CAN hardware
+ */
+bool cvnpHal_init() {
+
+}
