4dcdaf8dcecb8d38280c2e92b72656cd66078a85
BrianDuemmer <bduemmer1@gmail.com>
2019-04-15 18:56:43 -0400

    Added VCM and CVNP comm faults

--- src/hal/can_hal.c
+++ src/hal/can_hal.c
@@ -19,8 +19,8 @@
 #include "../cvnp/cvnp_config.h"
 #include "../hal/resource.h"
 
-#define CAN_TX_BUF_SIZE			3
-#define CAN_RX_BUF_SIZE			5
+#define CAN_RX_BUF_SIZE			6
+#define CAN_MAX_BUF_SIZE		32
 
 // Message buffers
 static uint8_t g_rxBuf[8], g_txBuf[8];
@@ -34,6 +34,9 @@ void _can_rxIntHandler() {
 }
 
 
+void _can_txIntHandler() {
+
+}
 
 
 void _can_tivaFrameToCVNP(tCanFrame *cvnp, tCANMsgObject *tiva) {
@@ -74,17 +77,9 @@ bool cvnpHal_init() {
 	CANIntRegister(CAN_RX_IFACE, _can_rxIntHandler);
 	CANInit(CAN_TX_IFACE);
 	CANBitRateSet(CAN_TX_IFACE, UTIL_CLOCK_SPEED, CVNP_BITRATE);
-	CANIntRegister(CAN_TX_IFACE, _can_txIntHandler);
 
-	// Prepare message objects
+	// Bind TX data buffer
 	g_txMsg.pui8MsgData = g_txBuf;
-	g_txMsg.ui32MsgIDMask = 0x0;
-	g_txMsg.ui32MsgLen = 8;
-	g_txMsg.ui32Flags = MSG_OBJ_TX_INT_ENABLE; // TODO: possibly need MSG_OBJ_EXTENDED_ID, but documentation is inconclusive
-
-	for(int i=0; i<CAN_TX_BUF_SIZE; i++) {
-		CANMessageSet(CAN_TX_IFACE, i, &g_txMsg, MSG_OBJ_TYPE_TX);
-	}
 
 	g_rxMsg.pui8MsgData = g_rxBuf;
 	g_rxMsg.ui32MsgIDMask = 0x0;
@@ -94,11 +89,36 @@ bool cvnpHal_init() {
 
 	for(int i=0; i<CAN_RX_BUF_SIZE; i++) {
 		CANMessageSet(CAN_RX_IFACE,
-					  i+CAN_TX_BUF_SIZE+1,
+					  i+1,
 					  &g_rxMsg,
 					  MSG_OBJ_TYPE_RX);
 	}
+
+	return true;
 }
+
+
+
+
+
+void cvnpHal_sendFrame(tCanFrame frame) {
+	_can_cvnpFrameToTiva(&frame, &g_txMsg);
+
+	// Find highest clear message ID
+	uint32_t txStat = CANStatusGet(CAN_TX_IFACE, CAN_STS_TXREQUEST);
+
+	int i=0;
+	while(i<32 && ((txStat >> i) & 1))
+		i++;
+
+	if(i>= 32) {
+		// TODO: Buffer full, possible VCM comm error
+	}
+
+	CANMessageSet(CAN_TX_IFACE, i, &g_txMsg, MSG_OBJ_TYPE_TX);
+}
+
+
 
 
 
